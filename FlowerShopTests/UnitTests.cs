ced5324e89e27e1c1221e1c0bf4d1fd854c8a3a4
sivesha <g16b0398@campus.ru.ac.za>
2019-02-23 18:24:32 +0200

    Flowershop update Order.cs amd UnitTests.cs

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -30,5 +30,25 @@ namespace Tests
 
 
         }
+        public void Test2()
+        {
+            //Arrange
+           
+            IOrderDAO d = Substitute.For<IOrderDAO>();
+            IClient a = Substitute.For<IClient>();
+
+            Flower f = new Flower(f,"daisy",60,6);
+            
+            List<Flower> flower = new List<Flower>();
+            flower.add(f);
+            IOrder o = new Order(y, c, false);
+
+
+            //Act
+            double price = o.Price;
+
+            //Assert
+            Assert.AreEqual(o.Price, 72);
+        }
     }
 }
