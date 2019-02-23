6c2897ed2b572576aa8d96ba49c696c9030e3067
sivesha <g16b0398@campus.ru.ac.za>
2019-02-23 14:23:36 +0200

    Added unit tests

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using Order;
+using IOrder;
+using Nsubstitute;
 
 namespace Tests
 {
@@ -12,7 +15,20 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+            //Arrange
+            IOrder o = Substitute.For<IOrder>();
+            IOrderDAO d = Substitute.For<IOrderDAO>();
+            IOrder o = new Order(y,c,false);
+            
+            //Act
+            o.IsDelivered = true;
+
+            //Assert
+            d.Received().SetDelivered(Arg.Any<IOrder>());
+
+
+
         }
     }
 }
